3ad72c6e40646ad6dcdfd181cb4fb44274d51048
jerome652 <castillojerome171@gmail.com>
2025-08-13 01:10:06 +0000

    main php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+<?php
+include 'main.php';
+?>
+
+</body>
+</html>
---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+$name = "jerome castillo";
+$age = "19";
+$school = "grc";
+$course = "bsit";
+$subject = "WEBSYS";
+
+echo "Name:" . $name . "<br>";
+echo "age:" . $age . "<br>";
+echo "school:" . $school . "<br>";
+echo "course:" . $course . "<br>";
+echo "subject:" . $subject . "<br>";
+
+echo "student: yes or no";
+
+
+
+?>
